5c76f3d8b73039886a577c7f1c85e480aa4cb763
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2026-01-30 00:21:37 +0600

    4th commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Navbar = () => {
       return (
             <div>
-                  
+                  <h2>This is Navbar</h2>
+                  <nav className='bg-gray-800 p-4'>
+                        <Link to="/" className='text-white mx-2'>Home</Link>
+                        <Link to="/mobiles" className='text-white mx-2'>Mobiles</Link>
+                        <Link to="/laptops" className='text-white mx-2'>Laptops</Link>
+                  </nav>
             </div>
       );
 };
